a0c85637197a1f8d1185d9b454831f2ace2884c6
wxb <wxb871127@gmail.com>
2019-02-21 17:44:25 +0800

    首次提交代码

---
+++ app/src/main/java/annotation/com/myannotation/MainActivity.java
@@ -0,0 +1,13 @@
+package annotation.com.myannotation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
